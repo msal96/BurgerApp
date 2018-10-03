15b6fae3780bb6b8ebd3855553efdaea9fbec3f0
msal96 <mihailsalari1996@yahoo.com>
2018-10-03 14:29:53 +0300

    Added order button + currentPrice calculation

--- burger-app/src/components/IngredientsSection.js
+++ burger-app/src/components/IngredientsSection.js
@@ -3,59 +3,66 @@ import styled from 'styled-components'
 
 export default class IngredientsSection extends Component {
   state = {
+    currentPrice: 0,
     salad: 0,
     bacon: 0,
     cheese: 0,
     meat: 0
   }
   onClickLess = (ingredient) => {
-    let value = 0
+    let amount = 0
+    let price = this.state.currentPrice
     switch (ingredient) {
       case 'salad':
-        value = this.state.salad - 1
-        this.setState({ salad: value })
+      amount = this.state.salad - 1
+        this.setState({ currentPrice: price - 1, salad: amount })
         break
       case 'bacon':
-        value = this.state.bacon - 1
-        this.setState({ bacon: value })
+      amount = this.state.bacon - 1
+        this.setState({ currentPrice: price - 3, bacon: amount })
         break
       case 'cheese':
-        value = this.state.cheese - 1
-        this.setState({ cheese: value })
+      amount = this.state.cheese - 1
+        this.setState({ currentPrice: price - 2, cheese: amount })
         break
       case 'meat':
-        value = this.state.meat - 1
-        this.setState({ meat: value })
+      amount = this.state.meat - 1
+        this.setState({ currentPrice: price - 4, meat: amount })
         break
       default: return
     }
   }
   onClickMore = (ingredient) => {
-    let value = 0
+    let amount = 0
+    let price = this.state.currentPrice
     switch (ingredient) {
       case 'salad':
-        value = this.state.salad + 1
-        this.setState({ salad: value })
+      amount = this.state.salad + 1
+        this.setState({ currentPrice: price + 1, salad: amount })
         break
       case 'bacon':
-        value = this.state.bacon + 1
-        this.setState({ bacon: value })
+      amount = this.state.bacon + 1
+        this.setState({ currentPrice: price + 3, bacon: amount })
         break
       case 'cheese':
-        value = this.state.cheese + 1
-        this.setState({ cheese: value })
+      amount = this.state.cheese + 1
+        this.setState({ currentPrice: price + 2, cheese: amount })
         break
       case 'meat':
-        value = this.state.meat + 1
-        this.setState({ meat: value })
+      amount = this.state.meat + 1
+        this.setState({ currentPrice: price + 4, meat: amount })
         break
       default: return
     }
   }
+  orderHandler = () => {
+    
+  }
   render() {
-    const { salad, bacon, cheese, meat } = this.state
+    const { currentPrice, salad, bacon, cheese, meat } = this.state
     return (
       <IngredientsSectionWrapper>
+        <CurrentPriceP>Current Price: {currentPrice}</CurrentPriceP>
         <IngredientWrapper>
           Salad: {salad}
           <button onClick={() => this.onClickLess('salad')}>Less</button>
@@ -79,6 +86,7 @@ export default class IngredientsSection extends Component {
           <button onClick={() => this.onClickLess('meat')}>Less</button>
           <button onClick={() => this.onClickMore('meat')}>More</button>
         </IngredientWrapper>
+        <OrderButton onClick={this.orderHandler}>ORDER NOW</OrderButton>
       </IngredientsSectionWrapper>
     )
   }
@@ -87,7 +95,7 @@ export default class IngredientsSection extends Component {
 const IngredientsSectionWrapper = styled.div`
   background-color: brown
   color: white
-  height: 10em
+  height: 12em
   width: 100%
   display: flex
   flex-direction: column
@@ -100,4 +108,13 @@ const IngredientWrapper = styled.div`
   width: 15%
   display: flex
   justify-content: space-between
+`
+const OrderButton = styled.button`
+  width: 100px
+  background-color: yellow
+  color: black
+  margin-bottom: 5px
+`
+const CurrentPriceP = styled.p`
+  color: black
 `
